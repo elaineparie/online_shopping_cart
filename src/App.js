ccbc57a955f6994fb9ddc12814d5c2fa43224e37
github username <github email address>
2019-02-28 12:33:22 -0500

    add homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Router from './Router';
 
 class App extends Component {
   render() {
     return <div>
     <h1>My Shopping Cart</h1>
+
+    <Router />
     </div>
   }
 }
