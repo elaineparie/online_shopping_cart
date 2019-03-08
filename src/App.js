56f83dcd189d7f2648635a4fbe22f6387674b7b5
github username <github email address>
2019-03-08 16:01:33 -0500

    add navlinks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 import Router from './Router';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = (props) => <nav>
+  <ul>
+  <li><NavLink to="/">Home</NavLink></li>
+  <li><NavLink to="/cart">Cart</NavLink></li>
+  </ul>
+</nav>
 
 class App extends Component {
   render() {
     return <div>
-    <h1>My Shopping Cart</h1>
+    <Navigation />
 
     <Router />
     </div>
